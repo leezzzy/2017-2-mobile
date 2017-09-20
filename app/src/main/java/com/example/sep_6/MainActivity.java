c1946ffe3d938a02ec5b4ee5d43598668d910466
leez <hnslzy@gmail.com>
2017-09-20 12:49:59 +0900

    1.1

--- app/src/main/java/com/example/sep_6/MainActivity.java
+++ app/src/main/java/com/example/sep_6/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.sep_6;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Scene;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Debug", "Hello this is the first");
     }
     public void nextScene(View v){
         Intent i = new Intent(this, Main2Activity.class);
         // Intent(from, to)
-            startActivity(i); // 새로운 액티비티 실행
+        startActivity(i); // 새로운 액티비티 실행
+//        ViewGroup vg = (ViewGroup) findViewById(R.id.lay2);
+//        Scene s = Scene.getSceneForLayout(vg, R.layout.activity_main2, this);
+//        s.enter();
+//        // scene은 시스템 생명주기를 따르지 않는다.
     }
 
 }
